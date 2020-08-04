90b5037198e1defd3ba08dd3781d2bce2fe1341a
Kyle Stough <kstough00@gmail.com>
2020-08-04 18:00:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ def prime?(num)
   if num <= 1 || num == 0 || num == 1
     return false
   elsif (2..num - 1).each do |i|
-    if num % i=
-    return true
+    if num % i == 0 
+    return false
+end
 end
